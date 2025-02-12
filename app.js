f0bc9af4ad724df5f2db0cf4d47876dba30ce3f2
Hectorg Higareda <hector.g.higareda@gmail.com>
2025-02-12 14:33:48 -0600

    Update app.js
    Aqui resovemos fallas

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-//Funcion para agregar un amigo al array 
-function agregarAmigo () {
-    const imputAmigo = document.getElementById("amigos");
-    const nombreAmigo = imputAmigo.value.trim();
+function agregarAmigo() {
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
 
     if (nombreAmigo === "") {
-        alert("Inserte un Nombre Valido.");
+        alert("Por favor, inserte un Nombre");
         return;
     }
 
     amigos.push(nombreAmigo);
 
-
     actualizarLista();
 
-//Limpiar campos
-    imputAmigo.value = "";
-    imputAmigo.focus();
+    inputAmigo.value = "";
+    inputAmigo.focus();
+
+
 }
-//Actualizamos lista de los amigos Ingresados.
+
 function actualizarLista() {
-    const listaAmigoUl = document.getElementById("listaAmigos");
+    const listaAmigoUl =document.getElementById("listaAmigos")
     listaAmigoUl.innerHTML = "";
-    
-    amigos.forEach(amigos => {
+
+    amigos.forEach(amigo => {
         const li =document.createElement("li");
         li.textContent = amigo;
-        listaAmigoUl.appendChild("li");
-    })
+        listaAmigoUl.appendChild(li);
+    });
 }
-//Funcion para el sorteo del amigo Secreto.
+
 function sortearAmigo() {
     if (amigos.length === 0) {
-        alert("No hay amigos registrados para sortear.");
-        return;
-    } 
-    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
-    
-    const amigoSorteados = amigos[indiceAleatorio];
-    
-    const resultadoUl = document.getElementById("resultado");
-    
-    resultadoUl.innerHTML = `<li>${amigoSorteados}</li>`;
+        alert("No hay amigos para sortear. Agrega nombres primero.");
+        return
+    }
 
-    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio]; 
+
+    const resultadoUl = document.getElementById("resultado");
+    resultadoUl.innerHTML = `<li>${amigoSorteado}</li>`;
+
 }
-//Etiquetado de botones.
-    document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("btnAdicional").addEventListener("click", agregarAmigo);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("btnAdicionar").addEventListener("click", agregarAmigo)
     document.getElementById("btnSortear").addEventListener("click", sortearAmigo);
 });
